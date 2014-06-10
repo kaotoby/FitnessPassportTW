4950489954eb900e47b6c19e7076722c6c58731e
kaotoby <toby850817@gmail.com>
2014-06-11 05:45:01 +0800

    main

---
+++ AutoSign-fitness/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace AutoSign_fitness
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.logtbx = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tryico = new System.Windows.Forms.NotifyIcon(this.components);
+            this.run_btn = new System.Windows.Forms.Button();
+            this.timep = new System.Windows.Forms.Panel();
+            this.mtbx = new System.Windows.Forms.NumericUpDown();
+            this.htbx = new System.Windows.Forms.NumericUpDown();
+            this.timep.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mtbx)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.htbx)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // logtbx
+            // 
+            this.logtbx.Location = new System.Drawing.Point(4, 75);
+            this.logtbx.Multiline = true;
+            this.logtbx.Name = "logtbx";
+            this.logtbx.ReadOnly = true;
+            this.logtbx.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.logtbx.Size = new System.Drawing.Size(386, 186);
+            this.logtbx.TabIndex = 4;
+            this.logtbx.WordWrap = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "hour";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "minute";
+            // 
+            // tryico
+            // 
+            this.tryico.Icon = ((System.Drawing.Icon)(resources.GetObject("tryico.Icon")));
+            this.tryico.Text = "notifyIcon1";
+            this.tryico.Visible = true;
+            this.tryico.MouseClick += new System.Windows.Forms.MouseEventHandler(this.tryico_MouseClick);
+            // 
+            // run_btn
+            // 
+            this.run_btn.Location = new System.Drawing.Point(215, 30);
+            this.run_btn.Name = "run_btn";
+            this.run_btn.Size = new System.Drawing.Size(103, 23);
+            this.run_btn.TabIndex = 7;
+            this.run_btn.Text = "RUN NOW!";
+            this.run_btn.UseVisualStyleBackColor = true;
+            this.run_btn.Click += new System.EventHandler(this.run_btn_Click);
+            // 
+            // timep
+            // 
+            this.timep.Controls.Add(this.mtbx);
+            this.timep.Controls.Add(this.htbx);
+            this.timep.Controls.Add(this.label2);
+            this.timep.Controls.Add(this.label1);
+            this.timep.Location = new System.Drawing.Point(97, 13);
+            this.timep.Name = "timep";
+            this.timep.Size = new System.Drawing.Size(103, 50);
+            this.timep.TabIndex = 8;
+            // 
+            // mtbx
+            // 
+            this.mtbx.Location = new System.Drawing.Point(54, 21);
+            this.mtbx.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.mtbx.Name = "mtbx";
+            this.mtbx.Size = new System.Drawing.Size(41, 20);
+            this.mtbx.TabIndex = 8;
+            this.mtbx.Value = new decimal(new int[] {
+            37,
+            0,
+            0,
+            0});
+            // 
+            // htbx
+            // 
+            this.htbx.Location = new System.Drawing.Point(7, 21);
+            this.htbx.Maximum = new decimal(new int[] {
+            23,
+            0,
+            0,
+            0});
+            this.htbx.Name = "htbx";
+            this.htbx.Size = new System.Drawing.Size(41, 20);
+            this.htbx.TabIndex = 7;
+            this.htbx.Value = new decimal(new int[] {
+            7,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 263);
+            this.Controls.Add(this.timep);
+            this.Controls.Add(this.run_btn);
+            this.Controls.Add(this.logtbx);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AutoSign-fitness";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.timep.ResumeLayout(false);
+            this.timep.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mtbx)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.htbx)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox logtbx;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NotifyIcon tryico;
+        private System.Windows.Forms.Button run_btn;
+        private System.Windows.Forms.Panel timep;
+        private System.Windows.Forms.NumericUpDown mtbx;
+        private System.Windows.Forms.NumericUpDown htbx;
+    }
+}
+
---
+++ AutoSign-fitness/Form1.cs
@@ -0,0 +1,357 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Web;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace AutoSign_fitness
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                this.tryico.Visible = true;
+            }
+        }
+
+        const string LOGIN_URL = "http://passport.fitness.org.tw/nlogin.aspx";
+        const string LOGOUT_URL = "http://passport.fitness.org.tw/loginout.aspx";
+        const string AJAX_URL = "http://passport.fitness.org.tw/master.passport.ashx";
+        CookieContainer Coa;
+        public delegate void EventHandler();
+        public event EventHandler PostErrorEvent;
+
+        public string Curnum { get; set; }
+        public string Curid { get; set; }
+        public string Curpass { get; set; }
+        public Thread MainThread { get; set; }
+        public string LastUrl { get; set; }
+        public string Lasthtml { get; set; }
+        private int LoginTimesCounter { get; set; }
+
+        public bool Inprocess { get; set; }
+        public void OnPostErrorEvent()
+        {
+            if (PostErrorEvent != null)
+                PostErrorEvent();
+        }
+
+        public void PostError_Event()
+        {
+            AddLog("POST error, retry in 2sec");
+            Thread.Sleep(2000);
+            if (LastUrl == LOGIN_URL)
+            {
+                Login(Curnum, Curid, Curpass);
+            }
+            else if (LastUrl == LOGOUT_URL)
+            {
+                Logout();
+            }
+            else
+            {
+                GetUserInfo(Lasthtml);
+            }
+        }
+
+
+        public delegate void StrDel(string str);
+        public void AddLog(string str)
+        {
+            if (logtbx.InvokeRequired)
+                logtbx.BeginInvoke(new StrDel(AddLog), new object[1] { str });
+            else
+            {
+                logtbx.AppendText(System.DateTime.Now.ToString() + ": " + str + "\r\n");
+                System.IO.File.AppendAllText(System.IO.Directory.GetCurrentDirectory() + "\\" + "log.log", System.DateTime.Now.ToString() + ": " + str + "\r\n", Encoding.UTF8);
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!File.Exists(System.IO.Directory.GetCurrentDirectory() + "\\" + "data.csv"))
+            {
+                MessageBox.Show("can not find the data.csv!");
+                Environment.Exit(0);
+            }
+            Inprocess = false;
+            Coa = new CookieContainer();
+            PostErrorEvent += new EventHandler(PostError_Event);
+            tryico.Text = this.Text;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //AddLog("ontimer");
+            if (!Inprocess)
+            {
+                if (DateTime.Now.Hour == htbx.Value && DateTime.Now.Minute == mtbx.Value)
+                {
+                    AddLog("Time Trigered");
+                    MainThread = new Thread(MainFunc);
+                    MainThread.Start();
+                }
+            }
+        }
+
+
+
+        private void MainFunc()
+        {
+            Inprocess = true;
+            StreamReader sr = new StreamReader(System.IO.Directory.GetCurrentDirectory() + "\\data.csv", Encoding.Default);
+            string s;
+            LoginTimesCounter = 0;
+
+            while ((s = sr.ReadLine()) != null)
+            {
+                if (s == "")
+                    continue;
+                string num = s.Split(',')[0];
+                string id = s.Split(',')[1];
+                string pass = s.Split(',')[2];
+                Login(num, id, pass);
+                //AddLog("after login sleep 5 times");
+                Thread.Sleep(2000);
+                Logout();
+                AddLog("logout");
+                //AddLog("after logout sleep 5 times");
+                Thread.Sleep(2000);
+            }
+            sr.Close();
+            Inprocess = false;
+            AddLog("<" + LoginTimesCounter + " users> login sucessfully!");
+        }
+
+
+        private string GetUserInfo(string str)
+        {
+            Regex reg1 = new Regex(@"id=""checksn"" value=""\d*\""");
+            Regex reg2 = new Regex(@"id=""checkmd5"" value=""[\W\w]*\""");
+            Regex reg3 = new Regex(@"id=""checktime"" value=""\d*\""");
+            Regex reg4 = new Regex(@"""LoginTimes"":""\d*""");
+
+            string sn = "", md5 = "", time = "", logintimes = "";
+
+            if (reg1.IsMatch(str))
+            {
+                sn = reg1.Match(str).Value.Split('"')[3];
+            }
+
+            if (reg2.IsMatch(str))
+            {
+                md5 = reg2.Match(str).Value.Split('"')[3];
+            }
+
+            if (reg3.IsMatch(str))
+            {
+                time = reg3.Match(str).Value.Split('"')[3];
+            }
+
+            if (sn != "" && md5 != "" && time != "")
+            {
+                Dictionary<string, string> postDict = new Dictionary<string, string>();
+                postDict.Add("checksn", sn);
+                postDict.Add("checkmd5", md5);
+                postDict.Add("checktime", time);
+                postDict.Add("checktarget", "");
+                string html = "";
+                CTRHttp(AJAX_URL, postDict, true, ref html);
+                if (reg4.IsMatch(html))
+                {
+                    logintimes = reg4.Match(html).Value.Split('"')[3];
+                    return logintimes;
+                }
+                else
+                    return null;
+            }
+            else
+                return null;
+
+        }
+
+        private void Login(string num, string id, string pass)
+        {
+            Curnum = num;
+            Curid = id;
+            Curpass = pass;
+
+            Random rx = new Random();
+            int xi = rx.Next(10, 30);
+            Random ry = new Random();
+            int yi = ry.Next(10, 30);
+
+            Dictionary<string, string> postDict = new Dictionary<string, string>();
+            postDict.Add("__EVENTVALIDATION", "/wEWBALvg+ixDAKspt6oDgLCu4iJCgKct7iSDGVTuTw5byt59/eKwKpXwmtFCzoa");
+            postDict.Add("__VIEWSTATE", "/wEPDwUKLTY4NzcyMTIyMg9kFgICAQ9kFgICBQ8PFgIeDU9uQ2xpZW50Q2xpY2sFDmxvYWRpbmdMb2NrKCk7ZGQYAQUeX19Db250cm9sc1JlcXVpcmVQb3N0QmFja0tleV9fFgEFB2J0blNhdmXXaIrDX3r8dIuhicIpnDBZkr2I9g==");
+            //postDict.Add("btnSave.x", "29");
+            //postDict.Add("btnSave.y", "18");
+            postDict.Add("btnSave.x", xi.ToString());
+            postDict.Add("btnSave.y", yi.ToString());
+            postDict.Add("txtST_PID", id);
+            postDict.Add("txtST_SITNO", pass);
+            string html = "";
+
+            CTRHttp(LOGIN_URL, postDict, true, ref html);
+
+            //html = System.Web.HttpUtility.HtmlDecode(html);
+
+            if (html.IndexOf("上次登入時間") != -1)
+            {
+                //AddLog("[no." + num + "] login ok!");
+                LoginTimesCounter++;
+                string logintimes = GetUserInfo(html);
+                if (logintimes != null)
+                {
+                    AddLog("[no." + num + "] login ok <" + logintimes + " times>");
+                }
+                else
+                    AddLog("[no." + num + "](" + id.Remove(7).Insert(7,"***") + ") login ok <get logintimes faild!>");
+            }
+            else
+            {
+                Regex r1 = new Regex(@"alert\([\w\W]*\)");
+                if (r1.IsMatch(html))
+                {
+                    AddLog("[no." + num + "](" + id + ") login faild!=>" + r1.Match(html).Value.Replace("alert", null));
+                }
+                else
+                    AddLog("id:[" + id + "]login faild!=>unknown error");
+            }
+
+        }
+
+        private void Logout()
+        {
+            string ts = "";
+            CTRHttp(LOGOUT_URL, null, false, ref ts);
+        }
+
+        public string quoteParas(Dictionary<string, string> paras)
+        {
+            string quotedParas = "";
+            bool isFirst = true;
+            string val = "";
+            foreach (string para in paras.Keys)
+            {
+                if (paras.TryGetValue(para, out val))
+                {
+                    if (isFirst)
+                    {
+                        isFirst = false;
+                        //quotedParas += para + "=" + HttpUtility.UrlPathEncode(val);
+                        quotedParas += para + "=" + System.Web.HttpUtility.UrlEncode(val);
+
+                    }
+                    else
+                        quotedParas += "&" + para + "=" + HttpUtility.UrlPathEncode(val);
+                }
+                else
+                    break;
+            }
+
+            return quotedParas;
+        }
+
+        public void CTRHttp(string url, Dictionary<string, string> postDict, bool GetResult, ref string result)
+        {
+            GC.Collect();
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            req.AllowAutoRedirect = true;
+            req.Accept = "image/gif, image/x-xbitmap, image/jpeg, image/pjpeg, */*";
+            req.KeepAlive = true;
+            req.AllowWriteStreamBuffering = true;
+            req.Credentials = System.Net.CredentialCache.DefaultCredentials;
+            req.MaximumResponseHeadersLength = -1;
+            req.ProtocolVersion = HttpVersion.Version10;
+            req.Headers.Add("Accept-Language", "zh-tw");
+            req.Headers.Add("Accept-Encoding", "gzip,deflate");
+            req.UserAgent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; Maxthon; .NET CLR 1.1.4322)";
+            req.Proxy = null;
+            req.Timeout = 30000;
+            req.ReadWriteTimeout = 30000;
+            req.CookieContainer = Coa;
+
+
+            try
+            {
+                if (postDict != null)
+                {
+                    string postDataStr = "";
+                    req.Method = "POST";
+                    req.ContentType = "application/x-www-form-urlencoded";
+                    postDataStr = quoteParas(postDict);
+                    byte[] postBytes = Encoding.UTF8.GetBytes(postDataStr);
+                    req.ContentLength = postBytes.Length;
+                    Stream postDataStream = req.GetRequestStream();
+                    postDataStream.Write(postBytes, 0, postBytes.Length);
+                    postDataStream.Close();
+                }
+                else
+                {
+                    req.Method = "GET";
+                }
+
+                HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+
+
+                if (GetResult)
+                {
+                    Stream stream = resp.GetResponseStream();
+                    var sr = new StreamReader(stream, Encoding.GetEncoding("UTF-8"));
+                    result = System.Web.HttpUtility.HtmlDecode(sr.ReadToEnd());
+                    Lasthtml = result;
+                }
+            }
+
+            catch (WebException e)
+            {
+                LastUrl = url;
+                OnPostErrorEvent();
+            }
+
+        }
+
+        private void tryico_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.Visible = true;
+            this.WindowState = FormWindowState.Normal;
+            this.tryico.Visible = false;
+        }
+
+        private void run_btn_Click(object sender, EventArgs e)
+        {
+            if (!Inprocess)
+            {
+                AddLog("Manual started");
+                MainThread = new Thread(MainFunc);
+                MainThread.Start();
+            }
+            /*else
+            {
+                timer1.Enabled = false;
+                timep.Enabled = true;
+                MainThread.Abort();
+                MainThread.Join(0);
+ 
+            }*/
+        }
+    }
+}
